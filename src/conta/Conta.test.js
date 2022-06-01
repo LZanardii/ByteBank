9ed9ab476afdaa802e10f6a822d1918ca03e875e
Leonardo Zanardi <leonardo.szanardi@gmail.com>
2022-06-01 19:08:55 -0300

    adicionando teste com jest.fn()

--- src/conta/Conta.test.js
+++ src/conta/Conta.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen } from "@testing-library/react"
+import { fireEvent, render, screen } from "@testing-library/react"
 import Conta from "./Conta";
 
 describe("Componetne CONTA", () => {
@@ -7,4 +7,14 @@ describe("Componetne CONTA", () => {
         render(<Conta saldo={1500}/>)
         expect(screen.getByTestId("saldo-conta").textContent).toBe("R$ 1500")
     })
+    
+    it("Deve chamar a função de realizao tranzação quando clicada", () => {
+        const realizarOperacaoButton = jest.fn()
+
+        render(<Conta saldo={1000} realizarTransacao={realizarOperacaoButton}/>)
+        fireEvent.click(screen.getByText("Realizar operação"))
+   
+        expect(realizarOperacaoButton).toHaveBeenCalled()
+    })
+
 })
